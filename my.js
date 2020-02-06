e51a023cdf7bdc1051702d396b54436821fc0929
Anton Dzyadevich <antonbilder86@gmail.com>
2020-02-06 21:50:34 +0300

    add: slider-html js css

--- my.js
+++ my.js
@@ -1,72 +1,76 @@
 "Use strict";
+//document.getElementById('text').onclick = function () {
+//  document.getElementById('text').hidden = true;
+//}
 
-//ДОМАШНЕЕ ЗАДАНИЕ ОБЬЕКТЫ, МЕТОДЫ.
+//let task = [];
+//if (localStorage.getItem('todo') != undefined) {
+//  task = JSON.parse (localStorage.getItem('todo'));
+//  out();
+//}
+//
+//document.getElementById('but').onclick = function () {
+//  let add = document.getElementById('text').value;
+//  let temp = {};
+//  temp.todo = add;
+//  temp.check = false;
+//  let i = task.length;
+//  task[i] = temp;
+//  console.log(task);
+//  out();
+//  localStorage.setItem('todo', JSON.stringify(task));
+//}
+//function out() {
+//  let out = '';
+//  for(let key in task) {
+//    if (task[key].check == true) {
+//      out += '<input type = "checkbox" checked>';
+//    }
+//    else {
+//     out += '<input type = "checkbox">';
+//  }
+//    out += task[key].todo + '<br>';
+//    
+//  }
+//  document.getElementById('out').innerHTML = out;
+//}
+//for (let li of document.querySelectorAll('li')) {
+//  console.log(li);
+//}
+//
+//for (let li of document.querySelectorAll('li')) {
+//  let title = li.innerHTML;
+//  console.log(title);
+//}
 
-let bodubuilder = {
-  name: "Ronny",
-  surname: "Coleman",
-  height: "180cm",
-  weight: "100kg",
-  training: false,
-  calories: 0,
+let btn_prev = document.querySelector('#gallery .buttons .prev');
+let btn_next = document.querySelector('#gallery .buttons .next');
+let images = document.querySelectorAll('#gallery .photos img');
+let i = 0;
+
+btn_prev.onclick = function() {
+  images[i].style.display = 'none';
+  i--;
   
-  add() {
-    if(this.calories == 0){
-      alert(`У ${this.name} нет сил, нужно поесть!`); 
-    }else {
-      this.training = true;
-    }
-  },
+  if(i < 0) {
+    i = images.length - 1;
+  }
+  images[i].style.display = 'block';
+}
+
+
+
+
+
+btn_next.onclick = function() {
+  images[i].style.display = 'none';
+  i++;
   
-  slim() {
-    this.training = false;
-  },
-  
-  eat() {
-    if(this.training) {
-      if(this.calories > 3800) {
-      alert(`${this.name} ${this.surname} набирает вес!`);
-      this.calories = this.calories - 1000;  
-      } else {
-      alert(`${this.name} ${this.surname} худеет!` );
-      this.slim();  
-      }  
-    }else {
-      alert(`${this.name} полон сил, пора начинать тренировку!`);
-    }  
-  },
-  
-  addCalories(amount) {
-    this.calories = this.calories + amount;
-  }  
-};
-//bodubuilder.add();
-//bodubuilder.eat();
-bodubuilder.add();
-bodubuilder.eat();
-bodubuilder.addCalories(4000);
-bodubuilder.add();
-bodubuilder.eat();
-//bodubuilder.eat();
-//bodubuilder.eat();
-//bodubuilder.slim();
-//console.log(bodubuilder.calories);
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
+  if(i >= images.length) {
+    i = 0;
+  }
+  images[i].style.display = 'block';
+}
 
 
 
